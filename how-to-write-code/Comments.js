d6ed739ff49e8b989ea01a41c480d26f12bc70d3
Malik Stuckey <stumali@amazon.com>
2024-07-13 23:07:50 -0400

    practice comments

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,12 +1,12 @@
-   // This demo shows off comments!
+// This demo shows off comments!
 
-   // console.log("This does not print.");
+console.log("This print now that I uncommented it.");
 
-   console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
+console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
-   /* Here is how
-   to have
-   multi-line
-   comments. */
+/* Here is how
+to havecd
+multi-line
+comments. */
 
-   console.log("Comments make your code more readable by others.");
+console.log("Comments make your code more readable by others.");
